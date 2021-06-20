bc44980e98b6a6439dbd3f62524458a183b28045
claudio-darkkenergy <claudio@darkkenergy.com>
2021-06-20 15:53:05 -0700

    Added  as a new method of the routing system.

--- src/router.ts
+++ src/router.ts
@@ -13,12 +13,25 @@ const { update } = historyApiActivity;
 // Hook into the History API onpopstate event when the browser history updates via back/forward controls.
 window.addEventListener('popstate', () => update(window.location, true));
 
+/**
+ * Hooks into the framework's routing system.
+ * @param routeConfigCallback An `ActivityHandler`.
+ * @returns A new `Node`.
+ */
 export const router = (routeConfigCallback: ActivityHandler<Location>) => {
     const { effect } = historyApiActivity;
     // Return the resolved route.
     return effect((props) => routeConfigCallback(props));
 };
 
+/**
+ * Use this to artificially update the browser `History` causing a navigation to occur.
+ * @param event A `MouseEvent` object.
+ * @param options Options should be passed when the event-target is not an `HTMLAnchorElement`.
+ *      Options properties accepted:
+ *          `href` - The href url to use - this overrides the href attribute of an `HTMLAnchorElement`.
+ *          `replace` - If set to `true`, "replaceState" will be used instead of "pushState" as the `History` action.
+ */
 export const onRoute = <T>(
     event?: SyntheticMouseEvent<T>,
     options?: OnRouteOptions
@@ -36,3 +49,17 @@ export const onRoute = <T>(
         update(window.location, true);
     }
 };
+
+/**
+ * Sanitizes the `Window.Location` object as follows:
+ *      `pathname` returns "/" or `/${path}` where the trailing slash is trimmed.
+ * @param location The current state of `Window.Location`.
+ * @returns A sanitized `Location` object shallow copy.
+ */
+export const sanitizeLocation = ({ pathname, ...loc }: Location) => ({
+    ...loc,
+    pathname:
+        pathname.length > 1 && pathname[pathname.length - 1] === '/'
+            ? pathname.slice(0, pathname.length - 1)
+            : pathname
+});
