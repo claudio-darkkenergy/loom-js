0894cfe76ee3d37744c8a5fd69d2fef2ef49e7e9
claudio-darkkenergy <claudio@darkkenergy.com>
2021-06-20 15:51:30 -0700

    Added  as a new method of .

--- src/activity.ts
+++ src/activity.ts
@@ -18,52 +18,66 @@ export const activity = <T>(initialValue?: T) => {
     >();
     const effect: ActivityEffect<T> = (action, cache = []) => {
         const ctxFunction = action({ value: initialValue });
+
+        if (typeof ctxFunction !== 'function') {
+            // Then `ctxFunction` is a pure `Node`, so just return it.
+            return ctxFunction;
+        }
+
         const ctx: TemplateContext = {};
         const node = ctxFunction(ctx);
 
         liveNodes.set(node, { action, cache, ctx });
         return node;
     };
+    const update = (newValue: T, force = false) => {
+        Array.from(liveNodes.entries()).forEach(
+            async ([liveNode, { action, cache, ctx }]) => {
+                if (
+                    document.contains(liveNode) &&
+                    (newValue !== currentValue || force)
+                ) {
+                    // Do the updates.
+                    let ctxFunction: ContextFunction;
+                    let node: Node;
+
+                    currentValue = newValue;
+                    ctxFunction = action({
+                        value: newValue
+                    }) as ContextFunction;
+                    // Render the component.
+                    node = ctxFunction(ctx);
+
+                    // If the new node is different than the live node, update the live node.
+                    if (node && !node.isSameNode(liveNode)) {
+                        liveNodes.set(node, {
+                            action,
+                            cache,
+                            ctx
+                        });
+                        // Replace the old node & perform cleanup on it for garbage colleciton.
+                        (liveNode as Element).replaceWith(node);
+                        liveNodes.delete(liveNode);
+                    }
+                } else {
+                    // Cleanup old nodes which have been removed from the DOM.
+                    liveNodes.delete(liveNode);
+                }
+            }
+        );
+    };
 
     return {
         effect,
+        // This method is reactive to the parent effect (or router), but not to it's own activity,
+        // & therefore is only to be used as a nested effect.
+        // The current value of the activity is always returned &
+        // because a component `ContextFunction` is returned instead of a `Node`,
+        // the result is always fully render a component each time it's called.
+        rawEffect: (action: ActivityHandler<T>) =>
+            (action({ value: currentValue }) as unknown) as Node,
         initialValue,
-        update(newValue: T, force = false) {
-            Array.from(liveNodes.entries()).forEach(
-                async ([liveNode, { action, cache, ctx }]) => {
-                    if (
-                        document.contains(liveNode) &&
-                        (newValue !== currentValue || force)
-                    ) {
-                        // Do the updates.
-                        let ctxFunction: ContextFunction;
-                        let node: Node;
-
-                        currentValue = newValue;
-                        ctxFunction = await action({ value: newValue });
-                        // Render the component.
-                        node = ctxFunction(ctx);
-
-                        // If the new node is different than the live node, update the live node.
-                        if (node && !node.isSameNode(liveNode)) {
-                            liveNodes.set(node, {
-                                action,
-                                cache,
-                                ctx
-                            });
-                            // Replace the old node & perform cleanup on it for garbage colleciton.
-                            (liveNode as Element).replaceWith(node);
-                            liveNodes.delete(liveNode);
-                        }
-                    } else {
-                        // Cleanup old nodes which have been removed from the DOM.
-                        liveNodes.delete(liveNode);
-                    }
-                }
-            );
-        },
-        value() {
-            return currentValue;
-        }
+        update,
+        value: () => currentValue
     };
 };
