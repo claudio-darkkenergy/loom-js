d2cb104a1360d4edf55d73ed72ebe75795c8e07d
Claudio Nunez Jr <claudio@darkkenergy.com>
2023-10-05 21:26:26 -0700

    Update `lazyImport` activity initial type to `undefined`.

--- packages/core/src/lazy-import.ts
+++ packages/core/src/lazy-import.ts
@@ -4,7 +4,8 @@ const lazyLoadedCache = new Map<string | Symbol, ReturnType<typeof activity>>();
 
 /**
  * Takes an import path & lazy loads a resource utilizing a subscribable activity.
- * @param path The import path
+ * @param key The cache key
+ * @param importer The import function
  * @returns An activity for the dynamic lazy import.
  */
 export const lazyImport = <ImportType>(
@@ -18,7 +19,7 @@ export const lazyImport = <ImportType>(
     }
 
     // Otherwise, create & cache a new activity for the import key & return.
-    const importActivity = activity<ImportType | {}>({});
+    const importActivity = activity<ImportType | undefined>(undefined);
 
     lazyLoadedCache.set(key, importActivity);
     importer().then(importActivity.update);
