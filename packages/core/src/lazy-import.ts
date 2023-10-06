894bfff441890a6bf3162f7e5b36fa4f9b655250
Claudio Nunez Jr <claudio@darkkenergy.com>
2023-10-05 23:25:52 -0700

    Fix `lazyImport` types.

--- packages/core/src/lazy-import.ts
+++ packages/core/src/lazy-import.ts
@@ -1,6 +1,8 @@
 import { activity } from './activity';
 
-const lazyLoadedCache = new Map<string | Symbol, ReturnType<typeof activity>>();
+type LazyImportCache<V> = Map<string | Symbol, ReturnType<typeof activity<V>>>;
+
+const lazyImportCache = new Map();
 
 /**
  * Takes an import path & lazy loads a resource utilizing a subscribable activity.
@@ -12,16 +14,18 @@ export const lazyImport = <ImportType>(
     key: string | Symbol,
     importer: () => Promise<ImportType>
 ) => {
+    const cache: LazyImportCache<ImportType | undefined> = lazyImportCache;
+
     // Check cache
-    if (lazyLoadedCache.has(key)) {
+    if (cache.has(key)) {
         // Return the cached activity for the import key.
-        return lazyLoadedCache.get(key) as ReturnType<typeof activity>;
+        return cache.get(key) as ReturnType<typeof activity>;
     }
 
     // Otherwise, create & cache a new activity for the import key & return.
     const importActivity = activity<ImportType | undefined>(undefined);
 
-    lazyLoadedCache.set(key, importActivity);
+    cache.set(key, importActivity);
     importer().then(importActivity.update);
 
     return importActivity;
