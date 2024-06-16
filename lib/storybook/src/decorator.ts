69d6c0fbc53b7572074851b62e1d4aaa90347544
claudio-darkkenergy <claudio@darkkenergy.com>
2024-06-15 22:53:08 -0700

    Add fixtures to storybook lib.

--- lib/storybook/src/decorator.ts
+++ lib/storybook/src/decorator.ts
@@ -1,15 +1,35 @@
 import { Decorator } from './types';
-import { init } from '@loom-js/core';
+import {
+    type Component,
+    type ContextFunction,
+    component,
+    init
+} from '@loom-js/core';
 
-export const LoomJsStorybookDecorator: Decorator = (story, ctx) => {
-    console.log({ ctx });
+const StoryAppRoot = component<{ story: Component }>(
+    (html, { attrs, story }) => html`
+        <div $attrs=${attrs}>${story()}</div>
+    `
+);
+
+export const LoomJsStorybookDecorator: Decorator = (
+    story,
+    { parameters: { decorator } }
+) => {
     const $storyContainer = document.createElement('div');
 
     $storyContainer.classList.add('story-container');
+
     init({
-        app: story(),
+        app: StoryAppRoot({
+            attrs: {
+                className: decorator?.className,
+                style: decorator?.style
+            },
+            story
+        }),
         root: $storyContainer
     });
 
-    return $storyContainer;
+    return $storyContainer as unknown as ContextFunction;
 };
