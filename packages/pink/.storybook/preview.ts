7e8eeb2d3cfab4304d592d54b21a985b19a2d88d
claudio-darkkenergy <claudio@darkkenergy.com>
2024-06-15 22:58:37 -0700

    Add loom-js storybook decorator to pink.

--- packages/pink/.storybook/preview.ts
+++ packages/pink/.storybook/preview.ts
@@ -1,14 +1,19 @@
+import '@appwrite.io/pink';
+import '@appwrite.io/pink-icons';
 import { LoomJsStorybookDecorator, type Preview } from '@loom-js/storybook';
 
+const { parameters } = (window as any).storybook;
+
 const preview: Preview = {
-    decorators: [LoomJsStorybookDecorator],
+    decorators: LoomJsStorybookDecorator,
     parameters: {
         controls: {
             matchers: {
                 color: /(background|color)$/i,
                 date: /Date$/i
             }
-        }
+        },
+        decorator: parameters.decorator.flex.center()
     }
 };
 
